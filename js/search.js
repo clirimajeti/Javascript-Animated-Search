4d11e25bf98d4002ea5c4521dff53e1df9dc8299
Çlirim Ajeti <36482517+clirimajeti@users.noreply.github.com>
2019-02-01 17:55:25 +0100

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,24 @@
+const searchBtn = document.getElementById('search-icon');
+const search = document.getElementById('search');
+const searchAnimation = document.getElementById('search-animation');
+const tip = document.getElementById('tip');
+
+searchBtn.addEventListener('click', ()=>{
+    search.style.animation = 'none';
+    search.style.width = '50%';
+    search.style.paddingLeft = '50px';
+    search.style.cursor = 'text';
+    search.focus();
+    searchAnimation.style.animation = 'none';
+});
+ search.addEventListener('blur',()=>{
+    search.style.width = '20px';
+    search.style.paddingLeft = '20px';
+    search.style.cursor = 'pointer';
+    search.value = "";
+    searchAnimation.style.animation = 'grow 1s infinite 2s';
+ });
+ search.addEventListener('keyup', () => {
+    tip.style.visibility = 'visible';
+    tip.style.opacity = '1';
+})
